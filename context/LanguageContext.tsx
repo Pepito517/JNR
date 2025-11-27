88c95910df8c39908b114458d1fbdcad2540b940
JosepNR97 <josepnunez97@gmail.com>
2025-11-27 18:50:20 +0100

    Idioma predeterminado español

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -13,7 +13,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('ca');
+  const [language, setLanguage] = useState<Language>('es');
 
   const value = {
     language,
